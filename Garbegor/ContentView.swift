809bd89cdf98916752df367b9515b5dfe5b0f54a
Ignas Davulis <ignas.davulis@swedbank.lt>
2023-11-26 20:26:34 +0200

    Initial Commit

---
+++ Garbegor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Garbegor
+//
+//  Created by Ignas Davulis on 2023-11-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Garbegor/GarbegorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GarbegorApp.swift
+//  Garbegor
+//
+//  Created by Ignas Davulis on 2023-11-26.
+//
+
+import SwiftUI
+
+@main
+struct GarbegorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
